d0ed8a1d4a2927259a88ac947573fdc303965b52
Derren Pamintuan <pamintuan2000@gmail.com>
2023-04-25 23:39:19 +0000

    Add an eventListener on submit for the search bar that fetches data from the API. Create new elements from the eventListener on submit to the table by using DOM creation. #1

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const $form = document.querySelector('form');
+const $input = document.querySelector('#search');
+const $tbody = document.querySelector('tbody');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const countryCode = $input.value;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://date.nager.at/api/v3/publicholidays/2023/' + countryCode);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    const data = xhr.response;
+
+    for (let i = 0; i < data.length; i++) {
+      const holiday = renderRow(data[i]);
+      $tbody.appendChild(holiday);
+    }
+  });
+  xhr.send();
+
+});
+
+function renderRow(holiday) {
+  const $date = document.createElement('td');
+  $date.setAttribute('class', 'date');
+
+  const $dateText = document.createTextNode(holiday.date);
+  $date.appendChild($dateText);
+
+  const $localName = document.createElement('td');
+  $localName.setAttribute('class', 'local-name');
+
+  const $localNameText = document.createTextNode(holiday.localName);
+  $localName.appendChild($localNameText);
+
+  const $name = document.createElement('td');
+  $name.setAttribute('class', 'name');
+
+  const $nameText = document.createTextNode(holiday.name);
+  $name.appendChild($nameText);
+
+  const $tr = document.createElement('tr');
+  $tr.append($date, $localName, $name);
+
+  return $tr;
+}
